49c318c7fae43f0f86a266600ade0d3af4e0438d
abhay-tank <abhay.tank@raweng.com>
2021-01-18 11:22:40 +0530

    Added theme

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styles from "./HomePage.module.scss";
+import logo from "../../assets/images/blog-feather-logo.svg";
 export default class HomePage extends Component {
 	static propTypes = {
 		user: PropTypes.object,
@@ -9,7 +10,8 @@ export default class HomePage extends Component {
 	render() {
 		return (
 			<div className={styles["container"]}>
-				<h1>Home Component</h1>
+				<img src={logo} alt="logo" />
+				<button className="btn">Get Started</button>
 			</div>
 		);
 	}
